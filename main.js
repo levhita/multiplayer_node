905d7c7c26e21a50d6521dff2cf4e616fdccd30f
Argel Arias <levhita@gmail.com>
2012-09-01 12:27:33 -0500

    Basic code from startupcycle

---
+++ main.js
@@ -0,0 +1,28 @@
+var express = require('express');
+
+var app = express();
+
+var messages = [],
+	ress     = [];
+
+
+app.get('/', function(req, res){
+  	res.send('Hello world');
+});
+
+app.get('/mensaje/new/:mensaje', function(req, res){
+	messages.push(req.params.mensaje);
+
+	ress.forEach(function(res){
+		res.send(messages+'<script>window.location.reload()</script>');
+	});
+
+	res.send('mensaje enviado');
+});
+
+app.get('/mensaje/list', function(req, res){
+	ress.push(res);
+});
+
+app.listen(3000);
+console.log("Express server running at\n  => http://localhost:3000/\nCTRL + C to shutdown");
